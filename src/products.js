3befe6d9ff41b645abdb38b8fb9f2f3285194cfc
Dhananjay Yadev <dhananjay16642@gmail.com>
2023-07-01 16:03:23 +0530

    updated

--- src/products.js
+++ src/products.js
@@ -1,7 +1,7 @@
 import product1 from "./assets/products/1.png";
-import product2 from "./assets/products/2.png";
+import product2 from "./assets/products/2.webp";
 import product3 from "./assets/products/3.png";
-import product4 from "./assets/products/4.png";
+import product4 from "./assets/products/4.webp";
 import product5 from "./assets/products/5.png";
 import product6 from "./assets/products/6.webp";
 import product7 from "./assets/products/7.webp";
@@ -16,7 +16,8 @@ export const PRODUCTS = [
   },
   {
     id: 2,
-    productName: "Macbook Pro 2022 (M1)",
+    productName:
+      "Watches Watch NIBOSI 2600 High Quality Men Automatic Mechanical Watches",
     price: 1999.0,
     productImage: product2,
   },
@@ -28,20 +29,22 @@ export const PRODUCTS = [
   },
   {
     id: 4,
-    productName: "WLS Van Gogh Denim Jacket",
-    price: 228.0,
+    productName:
+      "Rotation Metal Anti Theft Tablet Display Stand Holder Tablet Security Stand Tablet Pc Stand",
+    price: 9999.0,
     productImage: product4,
   },
   {
     id: 5,
-    productName: "LED Light Strips",
+    productName: "MacBook",
     price: 19.99,
     productImage: product5,
   },
   {
     id: 6,
-    productName: "SPECTRUM LS TEE",
-    price: 68.0,
+    productName:
+      "Mobile Adjustable Aluminum Alloy Flexible Universal Mobile Phone Stand Iphone Cell Laptop Mobile Phone ",
+    price: 299.0,
     productImage: product6,
   },
   {
